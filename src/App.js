e06d7542b8e84f01e62a1b65f92da3e4b4a9f8ae
Rosa Amor Dullas <dullasrosa@gmail.com>
2022-05-07 23:14:13 +0800

    commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,177 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {useState, useEffect} from 'react';
+import './App.css'
 
+import Header from './components/Header'
+import Form from './components/Form'
+import ToDoList from './components/ToDoList'
+
+  
 function App() {
+  
+  const apiUrl = 'http://localhost:3000/api/todo';
+
+  const [inputText, setInputText] = useState("")
+  const [todos, setTodos] = useState([])
+  const [status, setStatus] = useState("all");
+  const [filteredList, setFilteredList] = useState([])
+  const [isEditing, setIsEditing] = useState(null)
+  const [isGetting, setIsGetting] = useState(true)
+  const [isRefreshed, setIsRefreshed] = useState(false)
+
+  const [errorMessages, setErrorMessages] = useState({});
+  const [isSubmitted, setIsSubmitted] = useState(false);
+
+
+
+  const database = [
+    {
+      username: "admin",
+      password: "admin1234"
+    },
+  ];
+
+  const errors = {
+    uname: "invalid username",
+    pass: "invalid password"
+  };
+
+  useEffect(() => {
+    
+    if(isRefreshed === false)
+    {
+      fetch(apiUrl).then(res => {
+        return res.json();
+      }).then(r => {
+        setTodos(r)
+      })
+
+
+    }
+    
+  },[isGetting])
+
+  useEffect(() => {
+    filterHandler()
+  },[todos,status])
+
+  useEffect(() =>{
+    removeIsEditing()
+  },[todos,status,inputText])
+
+
+  const filterHandler = () => {
+    switch(status){
+      case "completed":
+        setFilteredList(todos.filter(todo => todo.completed === true))
+        break;
+      case "uncompleted":
+        setFilteredList(todos.filter(todo => todo.completed === false))
+        break;
+      default:
+        setFilteredList(todos);
+        break;
+    }
+  }
+
+  const removeIsEditing = () =>{
+    setIsEditing(null);
+  }
+
+  const fetchRefresh = () => {
+    setIsRefreshed(false)
+    setIsGetting(true)
+    setIsRefreshed(true)
+    setIsGetting(false)
+    
+  }
+
+  const handleSubmit = (event) => {
+
+    event.preventDefault();
+
+    var { uname, pass } = document.forms[0];
+
+    const userData = database.find((user) => user.username === uname.value);
+
+
+    if (userData) {
+      if (userData.password !== pass.value) {
+        setErrorMessages({ name: "pass", message: errors.pass });
+      } else {
+        setIsSubmitted(true);
+      }
+    } else {
+      setErrorMessages({ name: "uname", message: errors.uname });
+    }
+
+  };
+
+
+  const renderErrorMessage = (name) =>
+    name === errorMessages.name && (
+      <div className="error">{errorMessages.message}</div>
+    );
+
+
+ const renderForm = (
+  <div className="form"> 
+    <form onSubmit={handleSubmit}>
+    <div className='logheader'>
+      <header><b>TODO</b></header>
+      </div>
+      <div className="user-container">
+        <label><b>Username</b></label>
+        <input type="text" name="uname" required />
+        {renderErrorMessage("uname")}
+      </div>
+      <div className="password-container">
+        <label><b>Password</b></label>
+        <input type="password" name="pass" required />
+        {renderErrorMessage("pass")}
+      </div>
+      <div className="button-container">
+        <input type="submit" value="Login" />
+      </div>
+
+
+      
+    </form>
+    
+
+  </div>
+);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="login-form"> 
+        {isSubmitted ?  <div>
+         
+          <Header />
+      
+      
+
+      <Form inputText={inputText} setInputText={setInputText} 
+      todos={todos} setTodos={setTodos}
+      setStatus={setStatus}
+      apiUrl={apiUrl}
+      fetchRefresh={fetchRefresh}
+      />
+      <ToDoList setTodos={setTodos} todos={todos}
+       filteredList={filteredList}
+       isEditing={isEditing} setIsEditing={setIsEditing}
+       apiUrl = {apiUrl} 
+       fetchRefresh={fetchRefresh}
+       />
+
+    
+     
+        </div>:renderForm}
+        
+  </div>
+     </div>
+      
   );
 }
 
---
+++ src/components/ToDoList.js
@@ -0,0 +1,28 @@
+import Todo from './Todo'
+
+const TodoList = (props) => {
+    const taskNumber = props.filteredList.length
+    return (
+        <div className="todo-container">
+            <h2>{taskNumber > 0 ? taskNumber > 1 ? `${taskNumber} Tasks`
+            : `1 Task`
+            : "No Task Yet"}</h2>
+            <ul className="todo-list">
+                {props.filteredList.map(todo => (
+                    <Todo 
+                        setTodos={props.setTodos}
+                        todos={props.todos}
+                        todo={todo}
+                        key={todo.id} 
+                        text={todo.name}
+                        isEditing = {props.isEditing}
+                        setIsEditing = {props.setIsEditing}
+                        apiUrl = {props.apiUrl}
+                        fetchRefresh={props.fetchRefresh}/>
+                ))}
+            </ul>
+        </div>
+    )
+}
+
+export default TodoList
